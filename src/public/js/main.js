c104f24d5566c4d549c27dad9fa17f3bfee9b405
Ruben Padilla <yorubenyo@gmail.com>
2021-10-02 15:06:32 -0500

    se agregan fuunciones de cifrar y decifrar funcionando correctamente

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,3 +1,63 @@
+const ascii = [
+    "␀","␁","␂", "␃","␄",
+    "␅","␆", "␇", "␈", "␉",
+    "␊", "␋",  "␌",  "␍", "␎",
+    "␏", "␐", "␑", "␒","␓",    
+    "␔","␕", "␖", "␗","␘",
+    "␙", "␚", "␛", "␜", "␝",
+    "␞", "␟",  "␡", 
+    " ",  "!",   "\"", "#",    
+    "$",  "%",   "&",   "'",  "(",    
+    ")",  "*",   "+",   ",",  "-",    
+    ".",  "/",   "0",   "1",  "2",    
+    "3",  "4",   "5",   "6",  "7",    
+    "8",  "9",   ":",   ";",  "<",    
+    "=",  ">",   "?",   "@",  "A",    
+    "B",  "C",   "D",   "E",  "F",    
+    "G",  "H",   "I",   "J",  "K",    
+    "L",  "M",   "N",   "O",  "P",    
+    "Q",  "R",   "S",   "T",  "U",    
+    "V",  "W",   "X",   "Y",  "Z",    
+    "[",  "\\",  "]",   "^",  "_",    
+    "`",  "a",   "b",   "c",  "d",    
+    "e",  "f",   "g",   "h",  "i",    
+    "j",  "k",   "l",   "m",  "n",    
+    "o",  "p",   "q",   "r",  "s",    
+    "t",  "u",   "v",   "w",  "x",    
+    "y",  "z",   "{",   "|",  "}",    
+    "~",         "Ç",   "ü",  "é",
+    "â",  "ä",   "à",   "å",  "ç",
+    "ê",  "ë",   "è",   "ï",  "î",
+    "ì",  "Ä",   "Å",   "É",  "æ",
+    "Æ",  "ô",   "ö",   "ò",  "û",
+    "ù",  "ÿ",   "Ö",   "Ü",  "ø",
+    "£",  "Ø",   "×",   "ƒ",  "á",    
+    "í",  "ó",   "ú",   "ñ",  "Ñ",   
+    "ª",  "º",   "¿",   "®",  "¬",    
+    "½",  "¼",   "¡",   "«",  "»",    
+    "░",  "▒",   "▓",   "│",  "┤",
+    "Á",  "Â",   "À",   "©",  "╣",
+    "║",  "╗",   "╝",   "¢",  "¥",
+    "┐",  "└",   "┴",   "┬",  "├",    
+    "─",  "┼",   "ã",   "Ã",  "╚",
+    "╔",  "╩",   "╦",   "╠",  "═",
+    "╬",  "¤",   "ð",   "Ð",  "Ê",
+    "Ë",  "È",   "ı",   "Í",  "Î",
+    "Ï",  "┘",   "┌",   "█",  "▄",
+    "¦",  "Ì",   "▀",   "Ó",  "ß",
+    "Ô",  "Ò",   "õ",   "Õ",  "µ",
+    "þ",  "Þ",   "Ú",   "Û",  "Ù",
+    "ý",  "Ý",   "¯",   "´",  "≡",
+    "±",  "‗",   "¾",   "¶",  "§",
+    "÷",  "¸",   "°",   "¨",  "·",
+    "¹",  "³",   "²",   "■",  " "
+  ]
+
+const equivalencias = {
+    "0":0, "90":1, "180":0, //0 es igual a 180
+    "45":0,"135":1,
+}
+
 $(function(){
     const socket = io();
     //console.log('works!')
@@ -13,26 +73,96 @@ $(function(){
         console.log('sending data')
         var angulos = []
         for(let i=1;i<=10;i++){
-            angulos.push(Number($('#arrow_'+i).attr("alt")))
+            angulos.push($('#arrow_'+i).attr("alt"))
         }
-        const txt = $msg.val() //falta codificar el mensaje codifica($msg.val())
+        const txt = codifica($msg.val(),angulos) 
         $msg.val("");
         socket.emit('send message',txt) 
     })
 
     socket.on('new message', function(text){
-        $chat.append('<p id="msg_onchat">'+text+'</p>'+'<br>')
-        $("#msg_onchat").click(function(){
-            alert(text)
+        $chat.append('<p class="msg_onchat">'+text+'</p>'+'<br>')
+        $(".msg_onchat").click(function(){
+            var angulos = []
+            for(let i=1;i<=10;i++){
+                angulos.push($('#arrow_'+i).attr("alt"))
+            }
+            alert(decodifica(text,angulos))
         })
     })
 })
 
+function codifica(msg, array_angulos){
+    var palabras = [], res=""
+    
+    /*
+        De cada letra del mensaje 
+        obtiene los n bits correspondientes segun el ascii
+        cada bit se almacena en un array que se va 
+        en otro array de todas las letras
+    */
+    for(letra of msg){
+        const letra_on_binary = ascii.indexOf(letra).toString(2) 
+        palabras.push(letra_on_binary.split(""))  
+    }
+    // for(element of array_angulos){
+    //     console.log(element)
+    // }
+    // for(element of palabras){
+    //     console.log(element)
+    // }
+
+    /*
+    Se recorre la matriz multiplicando bit por bit
+    con las posiciones elegidas en las flechitas
+    */
+
+    for(word of palabras){
+        var letra_codificada = "", i = 0
+        for(element of word){
+            //console.log(`${element}^${equivalencias[array_angulos[i]]} = ${element ^ equivalencias[array_angulos[i]]}`)
+            letra_codificada += element ^ equivalencias[array_angulos[i++]] //cada elemento en binario se multiplica por el equivalente en binario del angulo
+        }
+        //console.log(`${letra_codificada} = ${binarioADecimal(letra_codificada)}`)
+        res += ascii[binarioADecimal(letra_codificada)]
+    }  
+
+    return res 
+}
+
+function decodifica(text,array_angulos){
+    var text_deco = "", code_words = []
+
+    for(letra of text){
+        const letra_on_binary = ascii.indexOf(letra).toString(2) 
+        code_words.push(letra_on_binary.split(""))  
+    }
+
+    for(word of code_words){
+        var letra_decodificada = "", i = 0
+        for(element of word){
+            letra_decodificada += element ^ equivalencias[array_angulos[i++]] //cada elemento en binario se multiplica por el equivalente en binario del angulo
+        }
+        text_deco += ascii[binarioADecimal(letra_decodificada)]
+    }
+
+    return text_deco
+}
+
+//funciones auxiliares 
+function binarioADecimal(num) {
+    let sum = 0;
+
+    for (let i = 0; i < num.length; i++) {
+       sum += +num[i] * 2 ** (num.length - 1 - i);
+    }
+    return sum;
+}
 function rotar(id){
     var val = Number($("#"+id).attr("alt")) + 45
     console.log(val)
     $("#"+id).css("transform",'rotate('+(val)+'deg)')
     $("#"+id).css("transition",".5s")
-    if(val>=360) val = 0
+    if(val>=180) val = 0
     $("#"+id).attr("alt",val)
 }
